52f4bbeb588c02e0378832043729a549f7d8abc5
Marius Usvat <marius.usvat@gmail.com>
2017-04-07 14:52:08 +0200

    Added default role assignment

--- common/models/member.js
+++ common/models/member.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = function(Member) {
-
+    Member.observe('after save', function assignDefaultRole(context, next) {
+        if (context.instance) {
+            Role.findOne({ where: { roleId: 3 } }, function(err, role) {
+                if (_.isEmpty(role)) {
+                    RoleMapping.findOne({ where: { principalId: userId } }, function(err, roleMapping){
+                        if (_.isEmpty(roleMapping)) {
+                            role.principals.create({ principalType: RoleMapping.USER, principalId: user.id }, function(err, principal) {
+                                if (err) { next(err); };
+                                next();
+                            });
+                        }
+                    });
+                }
+            });
+        }
+        
+        next();
+    });
 };
