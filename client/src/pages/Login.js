4dbdfa453d7e8ce73ac05b306e16f2ff9a421a38
Paul Boré <Paul Bor>
2017-04-07 09:53:16 +0200

    Integrated Login

---
+++ client/src/pages/Login.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+
+import '../styles/login.css';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            login_email: '',
+            login_pass: ''
+        }
+    }
+
+    onInputChange(event) {
+        let target = {};
+        target[event.target.id] = event.target.value;
+        this.setState(target);
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        axios.post('http://microcv.5unken.me/Members/login', {"email": this.state.login_email, "password": this.state.login_pass}).then(
+            success => { localStorage.setItem('token', success.data.id) },
+            fail => { alert("You suck"); }
+        );
+    }
+
+    render() {
+        return (
+            <div className="LoginPage">
+                <div className="login-html">
+                    <div className="login-form">
+                        <form onSubmit={this.onSubmit.bind(this)} className="sign-in-htm">
+                            <LoginInputGroup onChange={this.onInputChange.bind(this)} inputId="login_email" label="Adresse e-mail" type="text" />
+                            <LoginInputGroup onChange={this.onInputChange.bind(this)} inputId="login_pass" label="Mot de passe" type="password" />
+                            <div className="group">
+                                <input type="submit" className="button" value="Connexion" />
+                            </div>
+                            <div className="hr"></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+}
+
+class LoginInputGroup extends Component {
+    render() {
+        return (
+            <div className="group">
+                <label forHtml={this.props.inputId} className="label">{this.props.label}</label>
+                <input onChange={this.props.onChange} id={this.props.inputId} type={this.props.type} className="input" data-type={this.props.dataType} />
+            </div>
+        );
+    }
+}
+
+export default Login;
