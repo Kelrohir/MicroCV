23082225695f6d76865c277fd28f23d6ad89f1b8
Paul Boré <Paul Bor>
2017-04-07 17:36:00 +0200

    Addformation

--- client/src/pages/EditResume.js
+++ client/src/pages/EditResume.js
@@ -18,4 +18,49 @@ class EditResume extends Component {
     };
 }
 
+class EducationForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            start_date: '',
+            end_date: '',
+            title: ''
+         }
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        axios.post('http://microcv.5unken.me/Resume/{id}/education', {"startDate": this.state.start_date, "endDate": this.state.end_date, "title": this.state.title}).then(
+            success => {},
+            fail => {}
+        );
+    }
+
+    render() {
+        return (
+            <div className="EditResume-EducationForm">
+                <form onSubmit={this.onSubmit.bind(this)} className="Education-Form-htm">
+                        <EducationFormGroup onChange={this.onInputChange.bind(this)} inputId="start_date" label="Date de début de la formation" type="text" />
+                        <EducationFormGroup onChange={this.onInputChange.bind(this)} inputId="end_date" label="Date de fin de la formation" type="text" />
+                        <EducationFormGroup onChange={this.onInputChange.bind(this)} inputId="title" label="Intitulé de la formation" type="text" />
+                        <div className="group">
+                            <input type="submit" className="button" value="Soumettre" />
+                        </div>
+                </form>
+            </div>
+        );
+    };
+}
+
+class EducationFormGroup extends Component {
+    render() {
+        return (
+            <div className="group">
+                <label forHtml={this.props.inputId} className="label">{this.props.label}</label>
+                <input onChange={this.props.onChange} id={this.props.inputId} type={this.props.type} className="input" data-type={this.props.dataType} />
+            </div>
+        );
+    }
+}
+
 export default EditResume;
